494fd38ca09f575fbec189a6743f204fee16b5cc
Nadine Hernandez <nrhernandez93@gmail.com>
2019-10-27 17:23:26 -0400

    messed up

--- retail-api/src/main/java/com/company/retailapi/RetailApiApplication.java
+++ retail-api/src/main/java/com/company/retailapi/RetailApiApplication.java
@@ -1,10 +1,37 @@
 package com.company.retailapi;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCaching
+//@EnableRabbit
+@EnableCircuitBreaker
 public class RetailApiApplication {
+
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+		return rabbitTemplate;
+	}
+
+	@Bean
+	public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetailApiApplication.class, args);
